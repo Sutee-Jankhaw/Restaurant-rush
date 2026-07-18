5f23e4bb69eb09e4b7d3696513e209a53180b25c
Mark <sutee.j@igeargeek.com>
2026-07-18 17:40:49 +0700

    add Start page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
+import { Button } from './components/ui/button'
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-blue-400 text-white p-10">
-      Hello React
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className='flex flex-col gap gap-20'>
+        <header className='header'>
+          <h1>Restaurant Rush</h1>
+        </header>
+        <Button className="start" size="lg">Start</Button>
+      </div>
     </div>
   )
 }
