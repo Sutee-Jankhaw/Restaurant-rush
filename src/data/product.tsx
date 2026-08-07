5231da74333c259aa966f4ac08ce43346b8991c9
Mark <sutee.j@igeargeek.com>
2026-08-07 17:59:19 +0700

    add mocking data

---
+++ src/data/product.tsx
@@ -0,0 +1,60 @@
+import type { Product } from "@/type/product";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Hamburger",
+    price: 120,
+    category: "Food",
+    image: "/images/burger.png",
+  },
+  {
+    id: 2,
+    name: "French Fries",
+    price: 80,
+    category: "Food",
+    image: "/images/fries.png",
+  },
+  {
+    id: 3,
+    name: "Pizza",
+    price: 250,
+    category: "Food",
+    image: "/images/pizza.png",
+  },
+  {
+    id: 4,
+    name: "Fried Chicken",
+    price: 180,
+    category: "Food",
+    image: "/images/chicken.png",
+  },
+  {
+    id: 5,
+    name: "Hot Dog",
+    price: 100,
+    category: "Food",
+    image: "/images/hotdog.png",
+  },
+  {
+    id: 6,
+    name: "Cola",
+    price: 50,
+    category: "Drink",
+    image: "/images/cola.png",
+  },
+  {
+    id: 7,
+    name: "Orange Juice",
+    price: 60,
+    category: "Drink",
+    image: "/images/orange-juice.png",
+  },
+  {
+    id: 8,
+    name: "Ice Cream",
+    price: 90,
+    category: "Dessert",
+    image: "/images/ice-cream.png",
+  },
+];
