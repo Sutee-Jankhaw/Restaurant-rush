e155afcca698605b4b98345cd525a03432100a81
Mark <sutee.j@igeargeek.com>
2026-08-07 18:13:04 +0700

    add product card

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,13 +1,34 @@
+import { products } from "../data/product";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 function Game() {
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <div className='flex flex-col gap gap-20'>
-        <header className='header'>
-          <h1>Game Page</h1>
-        </header>
-      </div>
+    <div className="grid grid-cols-3 gap-6">
+      {products.map((product) => (
+        <Card key={product.id}>
+          <CardHeader>
+            <CardTitle>{product.name}</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <p>ราคา : {product.price} บาท</p>
+          </CardContent>
+
+          <CardFooter>
+            <Button className="w-full">
+              Add to Cart
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   )
 }
