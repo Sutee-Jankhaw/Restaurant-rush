39ffcd53c9548eac6dfa4a44551e7028c69b1c36
Mark <sutee.j@igeargeek.com>
2026-09-22 15:49:02 +0700

    add cart

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -7,6 +7,7 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 import type { Order, OrderItem } from "@/interfaces/order";
+import type { CartItem } from "@/interfaces/cart";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
@@ -39,11 +40,70 @@ const createOrder = (): Order => {
   };
   return newOrder;
 };
+
+const addToCart = (productId: number) => {
+  setCart((currentCart) => {
+    const existingItem = currentCart.find(
+      item => item.productId === productId
+    );
+    if (existingItem) {
+      return currentCart.map(item =>
+        item.productId === productId
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      );
+    } else {
+      return [
+        ...currentCart,
+        {
+          productId: productId,
+          quantity: 1,
+        },
+      ];
+    };
+  });
+};
+
+const increaseQuantity = (productId: number) => {
+  setCart((currentCart) => {
+    return currentCart.map(item =>
+      item.productId === productId 
+      ? {
+          ...item,
+          quantity: item.quantity + 1,
+        }
+      : item
+    );
+  });
+}
+const decreaseQuantity = (productId: number) => {
+  setCart((currentCart) => {
+    return currentCart.map(item =>
+      item.productId === productId 
+      ? {
+          ...item,
+          quantity: item.quantity - 1,
+        }
+      : item
+    );
+  });
+}
+const removeFromCart = (productId: number) => {
+  setCart((currentCart) =>
+    currentCart.filter(
+      (item) => item.productId !== productId
+    )
+  );
+};
+const [cart, setCart] = useState<CartItem[]>([]);
 const [order] = useState<Order>(() => createOrder());
 console.log(order)
   return (
     <div>
-      <div className="grid grid-cols-5 gap-6">
+      <div className="grid grid-cols-4 gap-6 m-10">
         <Card>
           <CardHeader>
             <CardTitle>Customer Order</CardTitle>
@@ -69,8 +129,60 @@ console.log(order)
             })}
           </CardContent>
         </Card>
+        <Card>
+        <CardHeader>
+          <CardTitle>Cart</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {cart.length === 0 ? (
+            <p>Cart is empty</p>
+          ) : (
+            cart.map((item) => {
+              const product = products.find(
+                product => product.id === item.productId
+              );
+
+              if (!product) return null;
+
+              return (
+                <div
+                  key={item.productId}
+                  className="flex items-center justify-between mb-3"
+                >
+                  <p className="font-bold">{product.name}</p>
+                  <p>จำนวน</p>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => decreaseQuantity(product.id)}
+                  >
+                    -
+                  </Button>
+                  <span>{item.quantity}</span>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => increaseQuantity(product.id)}
+                  >
+                    +
+                  </Button>
+                  <p>
+                    {product.price * item.quantity} บาท
+                  </p>
+                  <Button
+                    variant="destructive"
+                    onClick={() => removeFromCart(product.id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+                );
+              })
+            )}
+          </CardContent>
+        </Card>
       </div>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-3 gap-6 m-10">
         {products.map((product) => (
         <Card key={product.id}>
           <CardHeader>
@@ -82,7 +194,7 @@ console.log(order)
           </CardContent>
 
           <CardFooter>
-            <Button className="w-full">
+            <Button className="w-full" onClick={() => addToCart(product.id)}>
               Add to Cart
             </Button>
           </CardFooter>
