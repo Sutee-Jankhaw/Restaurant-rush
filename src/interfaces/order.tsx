9e413aa2ae4d61e0b75d87d80d2de0eda1ee5a74
Mark <sutee.j@igeargeek.com>
2026-09-22 13:59:31 +0700

    add random order

---
+++ src/interfaces/order.tsx
@@ -0,0 +1,9 @@
+export interface OrderItem {
+  productId: number;
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  items: OrderItem[];
+}
--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -6,13 +6,72 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import type { Order, OrderItem } from "@/interfaces/order";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 function Game() {
+const createOrder = (): Order => {
+  const items: OrderItem[] = [];
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(
+      Math.random() * products.length
+    );
+    const quantity = Math.floor(Math.random() * 3) + 1;
 
+    const product = products[randomIndex];
+
+    const existingItem = items.find(
+      item => item.productId === product.id
+    );
+    if (existingItem) {
+      existingItem.quantity += 1
+    } else {
+        items.push({
+        productId: products[randomIndex].id,
+        quantity: quantity,
+      });
+    }
+  }
+  const newOrder: Order = {
+    id: Date.now(),
+    items: items,
+  };
+  return newOrder;
+};
+const [order] = useState<Order>(() => createOrder());
+console.log(order)
   return (
-    <div className="grid grid-cols-3 gap-6">
-      {products.map((product) => (
+    <div>
+      <div className="grid grid-cols-5 gap-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>Customer Order</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            {order.items.map((item) => {
+              const product = products.find(
+              product => product.id === item.productId
+            );
+
+            if (!product) return null;
+
+            return (
+              <div key={item.productId} className="flex items-center gap-4">
+                <div>
+                  <p className="font-bold">{product.name}</p>
+                  <p>ราคา: {product.price} บาท</p>
+                  <p>จำนวน: {item.quantity}</p>
+                </div>
+              </div>
+              );
+            })}
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid grid-cols-3 gap-6">
+        {products.map((product) => (
         <Card key={product.id}>
           <CardHeader>
             <CardTitle>{product.name}</CardTitle>
@@ -29,6 +88,7 @@ function Game() {
           </CardFooter>
         </Card>
       ))}
+      </div>
     </div>
   )
 }
